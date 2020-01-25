4c240dc167caa1490eadb22a47653063c010d548
Archie Baer <archie@abaer.dev>
2020-01-25 23:23:30 +0000

    CORS

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+app.use(require("cors")());
 http.listen(8081);
 
 //Setup Functions
