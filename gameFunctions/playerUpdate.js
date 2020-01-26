42aa49769589c229ca8f059e51e645fb62ebf25c
Archie Baer <archie@abaer.dev>
2020-01-26 00:22:46 +0000

    Map Bounds and Death

--- gameFunctions/playerUpdate.js
+++ gameFunctions/playerUpdate.js
@@ -1,12 +1,29 @@
 const game = require("../gameData");
+const config = require("../config");
 const calculateMovement = require("../util/move");
 
 module.exports = () => {
     Object.keys(game.players).forEach(id => {
         const player = game.players[id];
+
+        //Movement
         const movement = calculateMovement(player.direction, player.speed * (player.speedBoost ? 2 : 1));
         player.x += movement.x;
         player.y += movement.y;
-        game.players[id] = player;
+
+        //Map Bounds
+        if (
+            player.x < 0 - config.mapSize / 2 ||
+            player.x > 0 + config.mapSize / 2 ||
+            player.y < 0 - config.mapSize / 2 ||
+            player.y > 0 + config.mapSize / 2
+        ) player.score--;
+
+        //Update
+        if (player.score > 0) {
+            game.players[id] = player;
+        } else {
+            delete game.players[id];
+        }
     });
 };
